cbc942fccc18769c086d0fca6d4ea62fc17c06f9
Kim Emery <kimberley.emery@gmail.com>
2019-08-22 16:14:24 +1200

    finished part 3 of PUN intro tutorial

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.MyCompany.MyGame
+{
+    public class GameManagerScript : MonoBehaviourPunCallbacks
+    {
+
+        #region Photon Callbacks
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+        #endregion
+
+        #region Public Methods
+
+        //abstracted so we can add more behaviour and features
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+        #endregion
+
+
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
